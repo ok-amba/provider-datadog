3ef0060a35eb30c01da89e3559eb528fe112ea1d
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2023-06-02 12:37:58 +0200

    added saml group mapping

--- apis/zz_register.go
+++ apis/zz_register.go
@@ -10,7 +10,8 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1alpha1 "github.com/ok-amba/provider-datadog/apis/dashboard/v1alpha1"
+	v1alpha1 "github.com/ok-amba/provider-datadog/apis/authnmapping/v1alpha1"
+	v1alpha1dashboard "github.com/ok-amba/provider-datadog/apis/dashboard/v1alpha1"
 	v1alpha1apis "github.com/ok-amba/provider-datadog/apis/v1alpha1"
 	v1beta1 "github.com/ok-amba/provider-datadog/apis/v1beta1"
 )
@@ -19,6 +20,7 @@ func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
 		v1alpha1.SchemeBuilder.AddToScheme,
+		v1alpha1dashboard.SchemeBuilder.AddToScheme,
 		v1alpha1apis.SchemeBuilder.AddToScheme,
 		v1beta1.SchemeBuilder.AddToScheme,
 	)
--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/upbound/upjet/pkg/controller"
 
+	authnmapping "github.com/ok-amba/provider-datadog/internal/controller/authnmapping/authnmapping"
 	dashboard "github.com/ok-amba/provider-datadog/internal/controller/dashboard/dashboard"
 	dashboardjson "github.com/ok-amba/provider-datadog/internal/controller/dashboard/dashboardjson"
 	providerconfig "github.com/ok-amba/provider-datadog/internal/controller/providerconfig"
@@ -18,6 +19,7 @@ import (
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		authnmapping.Setup,
 		dashboard.Setup,
 		dashboardjson.Setup,
 		providerconfig.Setup,
